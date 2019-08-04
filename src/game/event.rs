236b9915932fc689caface1a7a36b55f66abbdd4
Hanyang Xiao <46546073+Hanyang0901@users.noreply.github.com>
2019-08-03 22:33:18 -0700

    add some new enum

--- src/game/event.rs
+++ src/game/event.rs
@@ -19,3 +19,13 @@ pub struct MoveResult {
     pub source: BlockIndex,
     pub destination: BlockIndex
 }
+
+pub enum GameEvent {
+    ChangePiece {piece: Piece, guide_position: BlockIndexOffset},
+    MovePiece {piece: Piece, position: BlockIndexOffset, guide_position: BlockIndexOffset},
+    RemovePiece,
+    UpdateNextPieces(Vec<Piece>),
+    PutBlocks(Vec<PutResult>),
+    RemoveBlocks(Vec<RemoveResult>),
+    MoveBlocks(Vec<MoveResult>),
+}
