2d473c27a10d7a7c929adbdd5a098cc83d1a8981
haojian <haojian@pdx.edu>
2019-08-15 02:01:46 -0700

    delete files

--- target/debug/deps/test177-4bbff55fdad1842a.d
+++
@@ -1,5 +0,0 @@
-/Users/jianqianghao/Documents/GitHub/Tetris/target/debug/deps/test177-4bbff55fdad1842a.rmeta: src/main.rs
-
-/Users/jianqianghao/Documents/GitHub/Tetris/target/debug/deps/test177-4bbff55fdad1842a.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/test177-ece740661083e11a.d
+++
@@ -1,5 +0,0 @@
-/Users/jianqianghao/Documents/GitHub/Tetris/target/debug/deps/test177-ece740661083e11a.rmeta: src/main.rs
-
-/Users/jianqianghao/Documents/GitHub/Tetris/target/debug/deps/test177-ece740661083e11a.d: src/main.rs
-
-src/main.rs:
